c4ce0637476c845530aeb6879fe3dc2759790841
thedjbuntin <djbuntin@hotmail.co.uk>
2017-06-11 16:10:20 +0100

    Adjustments for GitHub Upload
    - Changed Include Path, now clearly D:\imgui
    - Added Extra Ignores for .VC.db

--- MusicPlayer/main.cpp
+++ MusicPlayer/main.cpp
@@ -10,31 +10,4 @@ int main()
 	application.Run();
 
 	return EXIT_SUCCESS;
-
-	/*
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	ImGui::SFML::Init(window);
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
-
-
-
-
-	while (window.isOpen())	// Game Loop
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			ImGui::SFML::ProcessEvent(event);
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		window.clear();
-		window.draw(shape);
-		window.display();
-	}
-
-	
-	return 0;*/
 }
