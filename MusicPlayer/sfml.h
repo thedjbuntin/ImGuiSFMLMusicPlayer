67d2f51eddee75ac2536a859e97da5587d925eeb
thedjbuntin <djbuntin@hotmail.co.uk>
2017-06-13 22:28:53 +0100

    Added SFML libs to release

--- MusicPlayer/sfml.h
+++ MusicPlayer/sfml.h
@@ -37,6 +37,18 @@ Loads appropriate SFML libs and dependencies
 		#pragma comment(lib,"sfml-audio-d.lib")
 		#pragma comment(lib,"sfml-window-d.lib")
 		#pragma comment(lib,"sfml-system-d.lib")
+
+		#pragma comment(lib,"opengl32.lib")
+		#pragma comment(lib,"freetype.lib")
+		#pragma comment(lib,"jpeg.lib")
+		#pragma comment(lib,"winmm.lib")
+		#pragma comment(lib,"gdi32.lib")
+		#pragma comment(lib,"openal32.lib")
+		#pragma comment(lib,"flac.lib")
+		#pragma comment(lib,"vorbisenc.lib")
+		#pragma comment(lib,"vorbisfile.lib")
+		#pragma comment(lib,"vorbis.lib")
+		#pragma comment(lib,"ogg.lib")
 	#endif
 	#pragma comment(lib,"sfml-main-d.lib")
 #else
@@ -62,6 +74,18 @@ Loads appropriate SFML libs and dependencies
 		#pragma comment(lib,"sfml-audio.lib")
 		#pragma comment(lib,"sfml-window.lib")
 		#pragma comment(lib,"sfml-system.lib")
+
+		#pragma comment(lib,"opengl32.lib")
+		#pragma comment(lib,"freetype.lib")
+		#pragma comment(lib,"jpeg.lib")
+		#pragma comment(lib,"winmm.lib")
+		#pragma comment(lib,"gdi32.lib")
+		#pragma comment(lib,"openal32.lib")
+		#pragma comment(lib,"flac.lib")
+		#pragma comment(lib,"vorbisenc.lib")
+		#pragma comment(lib,"vorbisfile.lib")
+		#pragma comment(lib,"vorbis.lib")
+		#pragma comment(lib,"ogg.lib")
 	#endif
 	#pragma comment(lib,"sfml-main.lib")
 #endif
